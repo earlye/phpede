795a88766bb741aae5a1bcbe53973e198f7545b3
Early Ehlinger <early.ehlinger@clearcapital.com>
2014-07-11 15:52:58 -0500

    - Added json_decode_throw module

---
+++ json_decode_throw.php
@@ -0,0 +1,17 @@
+<?php
+
+function json_decode_throw( $string )
+{
+  $object = json_decode( $string );
+  if (isset($object))
+    return $object;
+  switch( json_last_error() )
+    {
+    case JSON_ERROR_NONE:
+      return null; // the serialized object in $string was actually 'null'.
+    default:
+      throw new Exception( json_last_error_msg() );
+    }
+}
+
+?>
