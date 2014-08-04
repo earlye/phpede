486c6f3fc22b9692459e617c92f660bd7d8306f1
Early Ehlinger <earlye@gmail.com>
2014-08-04 10:10:47 -0500

    Added find_file_in_parent_directories

---
+++ find_file_in_parent_directories.php
@@ -0,0 +1,30 @@
+<?php
+
+// scan $first_directory and its parent directories until you find a
+// file with the specified basename, and return the path to that file.
+// return null if not found.
+function find_file_in_parent_directories( $basename , $first_directory )
+{
+  $dir = $first_directory;
+  if ( !isset( $dir ) )
+    $dir = pathinfo(__FILE__,PATHINFO_DIRNAME);
+
+  while(true)
+    {
+      $filename = $dir.DIRECTORY_SEPARATOR.$basename;
+      echo $filename."<br/>\n"; flush();
+      if ( file_exists( $filename ) )
+        {
+          return $filename;
+        }
+      $parent_dir = pathinfo($dir,PATHINFO_DIRNAME);
+      if( $parent_dir == $dir )
+        {
+          return null;
+        }
+      $dir = $parent_dir;
+    }
+}
+
+
+?>
--- phpede.php
+++ phpede.php
@@ -1,6 +1,7 @@
 <?php
 
 // Grab all files in the same directory as this module and require them, too.
+// In Emacs, try M-x, find-file-at-point on this string: "."
 foreach (glob(pathinfo( __FILE__ ,  PATHINFO_DIRNAME )."/*.php") as $filename)
   {
     require_once $filename;
