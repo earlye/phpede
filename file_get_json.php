060382242213708a7440d85d0257935859e6cb55
Early Ehlinger <early.ehlinger@clearcapital.com>
2014-11-26 10:19:25 -0600

    exceptions > trigger_error

--- file_get_json.php
+++ file_get_json.php
@@ -3,7 +3,7 @@
 function file_get_json( $filename )
 {
   if ( !file_exists( $filename ) )
-    trigger_error( "file_get_json failed: \"$filename\" does not exist." );
+    throw new exception( "file_get_json failed: \"$filename\" does not exist." );
   $contents = file_get_contents( $filename );
   return json_decode_throw( $contents );
 }
